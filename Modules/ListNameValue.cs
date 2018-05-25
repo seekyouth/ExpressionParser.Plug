27afb4b1ff3ebe138542ed8949ac27c2523d0e55
seekyouth <184457364@qq.com>
2018-05-25 17:28:50 +0800

    提交

---
+++ Modules/ListNameValue.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExpressionParser.Plug.Modules
+{
+    /// <summary>
+    /// 键值类
+    /// </summary>
+    public class ListNameValue
+    {
+        private string _value;
+
+        public string Value
+        {
+            get { return _value; }
+            set { _value = value; }
+        }
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public ListNameValue(string value, string name)
+        {
+            _value = value;
+            _name = name;
+        }
+    }
+}
